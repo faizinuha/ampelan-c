d8275304965a5f121697d09eb7debc4c88b5e180
TZ <rozakadm@gmail.com>
2025-06-05 15:13:30 +0700

    ❤  Ngebuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Profile from "./pages/Profile";
-import Admin from "./pages/Admin";
-import News from "./pages/News";
-import Activities from "./pages/Activities";
-import NotFound from "./pages/NotFound";
-import MainLayout from "./layouts/MainLayouts";
-import NotificationPermission from "./components/NotificationPermission";
-
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from '@/contexts/AuthContext';
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Admin from './pages/Admin';
+import News from './pages/News';
+import Activities from './pages/Activities';
+import NotFound from './pages/NotFound';
+import MainLayout from './layouts/MainLayouts';
+import NotificationPermission from './components/NotificationPermission';
+import CustomerService from './components/CustomerService';
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -33,6 +32,7 @@ const App = () => (
               <Route path="activities" element={<Activities />} />
               <Route path="profile" element={<Profile />} />
               <Route path="admin" element={<Admin />} />
+              <Route path="customer-service" element={<CustomerService />} />
             </Route>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
